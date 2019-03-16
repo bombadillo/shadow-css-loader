1801a1f7c5f1e86406fee86dc6229125200d0438
bombadillo <mumfordnsons@gmail.com>
2019-03-16 22:10:19 +0000

    Remove console.logs

--- src/index.js
+++ src/index.js
@@ -16,20 +16,12 @@ export default function(source) {
 
   validateOptions(schema, options, 'Example Loader');
 
-  console.log('doing some shit');
-
-  console.log(options);
-
   source = new CleanCSS({}).minify(source);
-  console.log(source);
 
   var injectShadowStyles = function(customElementTagName, source) {
     var customElementDomNode = document.querySelector(customElementTagName);
 
-    console.log(`trying to find ${customElementTagName}`);
-
     var injectCssIntoShadowRoot = shadowRoot => {
-      console.log(shadowRoot);
       var styleTag = document.createElement('style');
       styleTag.textContent = source;
       shadowRoot.appendChild(styleTag);
@@ -44,18 +36,14 @@ export default function(source) {
       }
 
       if (customElementDomNode.shadowRoot) {
-        console.log("we've got a shadow root, baby!");
         clearInterval(findShadowRootInterval);
         injectCssIntoShadowRoot(customElementDomNode.shadowRoot);
       } else {
-        console.log('no shadow root, dog');
         shadowRootDetectionAttemptCount++;
       }
     };
 
     var findShadowRootInterval = setInterval(detectShadowRootRendered, 1);
-
-    console.log(customElementDomNode);
   };
 
   return `export default String((${injectShadowStyles})("${
